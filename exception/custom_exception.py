200ab87254121acfd55b4704cdd0e04bad026dfc
Aryan <aryancoder03@gmail.com>
2025-08-11 18:42:39 +0530

    document analysis module completed

--- exception/custom_exception.py
+++ exception/custom_exception.py
@@ -1,21 +1,34 @@
 import sys
 import traceback
 from logger.custom_logger import CustomLogger
+
 logger = CustomLogger().get_logger("exception_experiment")
+
 class DocumentPortalException(Exception):
-    def __init__(self, error_message, error_details):
-        _, _, exc_tb = error_details.exc_info()
-        self.file_name = exc_tb.tb_frame.f_code.co_filename
-        self.line_no = exc_tb.tb_lineno
+    def __init__(self, error_message, error_details=None):
         self.error_message = str(error_message)
-        self.traceback_str = "".join(traceback.format_exception(*error_details.exc_info()))
+        self.file_name = None
+        self.line_no = None
+        self.traceback_str = None
+
+        if error_details is not None:
+            try:
+                _, _, exc_tb = error_details.exc_info()
+                if exc_tb:
+                    self.file_name = exc_tb.tb_frame.f_code.co_filename
+                    self.line_no = exc_tb.tb_lineno
+                    self.traceback_str = "".join(traceback.format_exception(*error_details.exc_info()))
+            except Exception as e:
+                logger.error(f"Failed to extract traceback info: {e}")
 
     def __str__(self):
-        return (
-            f"Error in [{self.file_name}] at line [{self.line_no}]\n"
-            f"Message: {self.error_message}\n"
-            f"Traceback: {self.traceback_str}"
-        )
+        details = []
+        if self.file_name and self.line_no:
+            details.append(f"Error in [{self.file_name}] at line [{self.line_no}]")
+        details.append(f"Message: {self.error_message}")
+        if self.traceback_str:
+            details.append(f"Traceback: {self.traceback_str}")
+        return "\n".join(details)
 
 if __name__ == "__main__":
     try:
