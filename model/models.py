bcf5568fa7646b0bc2ed0758cf7319cd3e4ed9db
Aryan <aryancoder03@gmail.com>
2025-08-11 18:17:23 +0530

    document analyzer code updated

--- model/models.py
+++ model/models.py
@@ -1,2 +1,12 @@
-from pydantic import BaseModel
-from typing import Optional,List,Dict,Any
+from pydantic import BaseModel,Field
+from typing import Optional,List,Dict,Any,Union
+class MetaData(BaseModel):
+    Summary: List[str] = Field(default_factory=list,description="summary of document")
+    Title: str
+    Author:str
+    DateCreated:str
+    LastModifiedDate:str
+    Publisher:str
+    Language:str
+    PageCount: Union[int,str]
+    SentimentTone: str
--- src/document_analyzer/data_analysis.py
+++ src/document_analyzer/data_analysis.py
@@ -1,18 +1,36 @@
 import os
+import sys
 from utils.model_loader import ModelLoader
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 from model.models import *
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
-
+from prompt.prompt_library import *
 class DocumentAnalyzer:
     """
     Analyzes documents using a pre-trained model
     Automatically logs all  actions and supports session-based organisations   
     """
-    def __init__():
-        pass
+    def __init__(self):
+        
+        self.log = CustomLogger().get_logger(__name__)
+        try:
+            self.loader = ModelLoader()
+            self.llm = self.loader.load_llm()
+
+            #Prepare parsers
+            self.parser = JsonOutputParser(pydantic_object=MetaData)
+            self.fixing_parser = OutputFixingParser.from_llm(parser = self.parser,llm = self.llm)
+            self.prompt = document_analysis_prompt
+            self.log.info("Document Analyser initialised successfully")
+
+
+        except Exception as e:
+            self.log.error(f"Error initialising Document Analyzer:{e}")
+            raise DocumentPortalException("Error in DocumentAnalyzer Initialisation",sys)
+        
+
 
     def analyze_metadata():
-        
+        pass
