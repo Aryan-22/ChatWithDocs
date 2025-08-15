f53a39875ddf511c52d4c18c4cbb90249a5bc330
Aryan <aryancoder03@gmail.com>
2025-08-15 16:41:14 +0530

    folder structure updated

---
+++ archive/src/document_compare/data_ingestion.py
@@ -0,0 +1,106 @@
+import sys
+import os
+from pathlib import Path
+import fitz
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from datetime import datetime,timezone
+import uuid
+class DocumentIngestion:
+    def __init__(self,base_dir:str = "data\document_compare",session_id = None):
+        self.log = CustomLogger().get_logger(__name__)
+        self.base_dir = Path(base_dir)
+        self.session_id = session_id or f"session_{datetime.now(timezone.utc).strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4().hex[:8]}"
+        self.session_path = self.base_dir / self.session_id
+        self.session_path.mkdir(parents=True,exist_ok=True)
+        self.log.info("DocumentIngestion initialised.",session_path = str(self.session_path))
+    
+        
+        
+
+        
+
+    def save_uploaded_files(self,reference_file,actual_file):
+        """
+        Saves reference and actual PDF files in the session directory.
+        """
+        try:
+            ref_path = self.base_dir/ reference_file.name
+
+            act_path = self.base_dir/ actual_file.name
+            if not reference_file.name.endswith(".pdf") or not actual_file.name.endswith(".pdf"):
+                raise ValueError("Only PDFs are allowed.")
+            with open(ref_path,"wb") as f:
+                f.write(reference_file.getbuffer())
+            with open(act_path,"wb") as f:
+                f.write(actual_file.getbuffer())
+
+            self.log.info("Files saved",reference = str(ref_path),actual = str(act_path))
+            return ref_path,act_path
+
+        except Exception as e:
+            self.log.error(f"Error saving the uploaded file: {e}")
+            raise DocumentPortalException("An error occured while saving uploaded files.",sys)
+        
+    def read_pdf(self,pdf_path:Path) -> str:
+        """
+        Reads a PDF file and extracts text from each page.
+        """
+
+        try:
+            with fitz.open(pdf_path) as doc:
+                if doc.is_encrypted:
+                    raise ValueError(f"PDF is encrypted: {pdf_path.name}")
+                
+                all_text = []
+                for page_num in range(doc.page_count):
+                    page = doc.load_page(page_num)
+                    text = page.get_text()
+                    if text.strip():
+                        all_text.append(f"\n --- Page{page_num + 1} ---\n{text}")
+                    
+                self.log.info("PDF read successfully",file = str(pdf_path),pages = len(all_text))
+                return "\n".join(all_text)
+            
+        except Exception as e:
+            self.log.error(f"Error reading PDF: {e}")
+            raise DocumentPortalException("An error occured while reading the PDF.",sys)
+        
+
+    def combine_documents(self)->str:
+        try:
+            content_dict = {}
+            doc_parts = []
+            for filename in sorted(self.base_dir.iterdir()):
+                if filename.is_file() and filename.suffix == ".pdf":
+                    content_dict[filename.name] = self.read_pdf(filename)
+
+            for filename,content in content_dict.items():
+                doc_parts.append(f"Document: {filename}\n{content}")
+
+            combined_text = "\n\n".join(doc_parts)
+            self.log.info("Documents combined",count = len(doc_parts))
+            return combined_text
+        except Exception as e:
+            self.log.error(f"Error combining documents: {e}")
+            raise DocumentPortalException("An error occurred while combining documents.",sys)
+        
+    def clean_old_sessions(self,keep_latest:int = 3):
+        """
+        Optional method to delete older session folders,keeping only the latest N.
+        """
+        try:
+            session_folders = sorted(
+                [f for f in self.base_dir.iterdir() if f.is_dir()],
+                reverse = True
+            )
+
+            for folder in session_folders[keep_latest:]:
+                for file in folder.iterdir():
+                    file.unlink()
+                folder.rmdir()
+                self.log.info("Old session folder deleted",path = str(folder))
+        except Exception as e:
+            self.log.error("Error cleaning old session",error = str(e))
+            raise DocumentPortalException("Error in cleaning old sessions",sys)
+        
---
+++ archive/src/document_compare/document_comparator.py
@@ -0,0 +1,60 @@
+import sys
+from dotenv import load_dotenv
+import pandas as pd
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from model.models import *
+from prompt.prompt_library import PROMPT_REGISTRY
+from utils.model_loader import ModelLoader
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import OutputFixingParser
+
+class DocumentComparatorLLM:
+    def __init__(self):
+        
+        self.log = CustomLogger().get_logger(__name__)
+
+        self.loader = ModelLoader()
+        self.llm = self.loader.load_llm()
+
+        #Prepare parsers
+        self.parser = JsonOutputParser(pydantic_object=SummaryResponse)
+        self.fixing_parser = OutputFixingParser.from_llm(parser = self.parser,llm = self.llm)
+        self.prompt = PROMPT_REGISTRY[PromptType.DOCUMENT_COMPARISON.value]
+        
+        self.chain = self.prompt | self.llm | self.parser
+
+        self.log.info("DocumentComparatorLLM initialised with model and parser.")
+
+
+    def compare_documents(self,combined_docs)-> pd.DataFrame:
+        """
+        Compares two documents and returns a structured comparison.
+        """
+        try:
+            inputs = {
+
+                "combined_docs":combined_docs,
+                "format_instruction":self.parser.get_format_instructions()
+            }
+            self.log.info(f"Starting document comparison",inputs = inputs)
+            response = self.chain.invoke(inputs)
+            return self._format_response(response)
+        except Exception as e:
+            self.log.error(f"Error in compare_documents: {e}")
+            raise DocumentPortalException("An error occured while comparing documents",sys)
+        
+
+    def _format_response(self,response_parsed:list[dict]) -> pd.DataFrame:
+        """
+        Formats the response from the LLM into a structured format.
+        """
+        try:
+            df = pd.DataFrame(response_parsed)
+            self.log.info("Response formatted into DataFrame",dataframe = df)
+            return df
+
+            
+        except Exception as e:
+            self.log.error(f"Error in formatting response into DataFrame: {e}")
+            raise DocumentPortalException("An error occured while comparing documents",sys)
